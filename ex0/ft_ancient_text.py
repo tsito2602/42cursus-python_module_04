8b44f5b03de45f22f4b52b6aa7024f92be39fda5
bassaaaa <white.galaxys2.album2@gmail.com>
2026-05-31 15:09:59 +0900

    ex0を追加

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,27 @@
+import sys
+import typing
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("Usage: ft_ancient_text.py <file>")
+        return
+    filename = sys.argv[1]
+
+    print("=== Cyber Archives Recovery ===")
+    print(f"Accessing file '{filename}'")
+    try:
+        file: typing.IO[str] = open(filename, "r")
+    except FileNotFoundError as e:
+        print(f"Error opening file '{filename}': {e}")
+    except PermissionError as e:
+        print(f"Error opening file '{filename}': {e}")
+    else:
+        content = file.read()
+        print("---\n\n" + content + "\n---")
+        file.close()
+        print(f"File '{filename}' closed.")
+
+
+if __name__ == "__main__":
+    main()
